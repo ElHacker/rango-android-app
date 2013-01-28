ad39a4add914cbdba3c8ad2db138182b0df7644c
syred <pataslargas27@gmail.com>
2013-01-27 16:31:03 -0800

    Init working with holo ui

---
+++ src/com/sutil/rango/FragmentTwo.java
@@ -0,0 +1,43 @@
+package com.sutil.rango;
+
+import org.holoeverywhere.LayoutInflater;
+import org.holoeverywhere.app.Fragment;
+import org.holoeverywhere.widget.TextView;
+
+import com.example.holotest.R;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentTwo extends Fragment {
+	TextView showMessage;
+	Context context;
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+		
+		context = getActivity();
+	}
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	        Bundle savedInstanceState) {
+	    View v = inflater.inflate(R.layout.fragment_two, container, false);
+
+	    return  v;
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+
+}
