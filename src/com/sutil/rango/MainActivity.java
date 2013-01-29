81d31c3ff8ab1adbe9f79cbb4b4b2a3c5a16f894
syred <pataslargas27@gmail.com>
2013-01-28 21:07:24 -0800

    Added minimum viable facebook login with facebook android sdk

--- src/com/sutil/rango/MainActivity.java
+++ src/com/sutil/rango/MainActivity.java
@@ -5,6 +5,10 @@ import org.holoeverywhere.widget.TextView;
 import com.actionbarsherlock.app.ActionBar;
 import com.example.holotest.R;
 
+import com.facebook.*;
+import com.facebook.model.*;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.WindowManager;
@@ -25,6 +29,32 @@ public class MainActivity extends Activity {
 	    mViewPager.setId(R.id.pager);
 	    setContentView(mViewPager);
 	    
+	    // Start Facebook Login
+	    Session.openActiveSession(this, true, new Session.StatusCallback() {
+			
+	    	// Callback when session changes state
+			@Override
+			public void call(Session session, SessionState state, Exception exception) {
+				if (session.isOpened()) {
+					
+					// make request to the /me API
+					Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+						
+						// Callback when Graph API response with user object
+						@Override
+						public void onCompleted(GraphUser user, Response response) {
+							if(user != null) {
+								TextView welcome = (TextView) findViewById(R.id.textView2);
+								welcome.setText("Hello " + user.getName() + "!");
+								welcome.refreshDrawableState();
+							}							
+						}
+					});
+					
+				}
+			}
+		});
+	    
 	    // Create action bar
 	    ActionBar bar = getSupportActionBar();
 	    bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -43,5 +73,11 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onPause() {
 		super.onPause();
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
 	}
 }
