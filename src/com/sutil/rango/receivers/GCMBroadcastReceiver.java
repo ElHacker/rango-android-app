e3d56f9553d3dd9db895365c3069bf6041aff45a
Braulio Chavez <pataslargas27@gmail.com>
2013-06-23 19:38:30 -0700

    [#48259185] Como usuario quiero recibir notificaciones cuando tenga una invitación de amistad

--- src/com/sutil/rango/receivers/GCMBroadcastReceiver.java
+++ src/com/sutil/rango/receivers/GCMBroadcastReceiver.java
@@ -4,6 +4,9 @@ import org.holoeverywhere.app.Activity;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.sutil.rango.IncomingCallActivity;
+import com.sutil.rango.R;
+import com.sutil.rango.RequestsListActivity;
+import com.sutil.rango.lib.NotificationSetter;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -29,10 +32,25 @@ public class GCMBroadcastReceiver extends BroadcastReceiver {
 		} else {
 			Log.d(TAG, "Message Received: " + intentExtrasMsg);
 			Bundle intentExtras = intent.getExtras();
-			Intent incominCallIntent = new Intent(context, IncomingCallActivity.class);
-			incominCallIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			incominCallIntent.putExtras(intentExtras);
-			ctx.startActivity(incominCallIntent);
+			String action  = intentExtras.getString("action");
+			if (action != null) {
+				if (action.equals("call")) {
+					// Incoming call
+					Intent incominCallIntent = new Intent(context, IncomingCallActivity.class);
+					incominCallIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+					incominCallIntent.putExtras(intentExtras);
+					ctx.startActivity(incominCallIntent);	
+				} else if (action.equals("invite")) {
+					// Friendship request
+					Intent notificationIntent = new Intent(context, RequestsListActivity.class);
+					int icon = R.drawable.notification;
+					String title = intentExtras.getString("title");
+					String message = intentExtras.getString("message");
+					String ticker_text = "Invitación de amistad";
+					NotificationSetter notification_setter = new NotificationSetter(context);
+					notification_setter.setNotification(notificationIntent, title, ticker_text, message, icon);
+				}
+			}
 		}
 		setResultCode(Activity.RESULT_OK);
 	}
